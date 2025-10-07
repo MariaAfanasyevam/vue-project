f7e349ac8cc3c59c0b7e6dc33dc91b3e69a6a841
MariaAfanasievam <afanmaria@mail.ru>
2025-10-07 12:49:11 +0300

    new project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="rows-container">
+      <div class="input-row">
+        <input type="text" />
+      </div>
+      <div class="input-row">
+        <input type="text" />
+      </div>
+      <div class="input-row">
+        <input type="text" />
+      </div>
+      <div class="button">
+        <button>Сохранить</button>
+      </div>
+    </div>
+  </div>
+</template>
+<style src="./style/style.css"></style>
