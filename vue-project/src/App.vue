5db1fa84d0c01325aa538cb3dba9334b7e551b35
MariaAfanasievam <afanmaria@mail.ru>
2025-10-07 16:16:39 +0300

    fix localstorage

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 
 const price = ref(0);
 const quantity = ref(0);
 const amount = ref(0);
 const counter = ref(0);
-
+const changedInput = ref("");
 const events = ref([]);
 const localStorageInfo = computed(() => {
   const data = {
@@ -27,24 +27,99 @@ const debounce = (f, delay) => {
   };
 };
 
+const logEvent = (type, body) => {
+  const event = {
+    id: Date.now(),
+    type,
+    body,
+    time: new Date().toLocaleTimeString(),
+  };
+
+  events.value.unshift(event);
+};
+const onChangePrice = debounce(() => {
+  changedInput.value = "price";
+  logEvent("Изменено значение в поле Цена", `Цена изменена на ${price.value}`);
+}, 300);
+
+const onChangeQuantity = debounce(() => {
+  changeInput.value = "quantity";
+  logEvent(
+    "Изменено значение в поле Количество",
+    `Количество изменено на ${quantity.value}`,
+  );
+}, 300);
+
+const onChangeAmount = debounce(() => {
+  changedInput.value = "amount";
+  logEvent(
+    "Изменено значение в поле Amount",
+    `Сумма изменена на ${amount.value}`,
+  );
+}, 300);
+const onSave = async () => {
+  counter.value += 1;
+  const datatoLocal = {
+    counter: counter.value,
+    price: price.value,
+    quantity: quantity.value,
+    amount: amount.value,
+  };
+  let localStorageData = localStorage.getItem("localData");
+  let oldLocalData = JSON.parse(localStorageData);
+  logEvent(
+    "Отправка данных",
+    `Отправлено: ${JSON.stringify(datatoLocal)}, значение в localStorage: ${JSON.stringify(oldLocalData, null, 2)}`,
+  );
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  let parsedLocalData = JSON.parse(localStorageData);
+  const success = amount.value % 2 === 0;
+  if (success) {
+    localStorage.setItem("localData", JSON.stringify(datatoLocal));
+    const updatedLocalStorageData = localStorage.getItem("localData");
+    logEvent(
+      "Сообщение от сервера",
+      `Сохранено: ${JSON.stringify(datatoLocal)},  значение в localStorage:${updatedLocalStorageData}}`,
+    );
+  } else {
+    logEvent(
+      "Сообщение от сервера",
+      `Ошибка сохранения: ${JSON.stringify(datatoLocal)},  значение в localStorage: ${JSON.stringify(parsedLocalData, null, 2)}`,
+    );
+  }
+};
+
+const calculate = () => {
+  if (changedInput.value === "price") {
+    amount.value = price.value * quantity.value;
+  } else if (changedInput.value === "quantity") {
+    amount.value = price.value * quantity.value;
+  } else if (changedInput.value === "amount") {
+    if (quantity.value !== 0) {
+      price.value = amount.value / quantity.value;
+    }
+  }
+};
+
+watch([price, amount, quantity], () => {
+  if (changedInput.value) {
+    calculate();
+  }
+});
 onMounted(() => {
   const localData = localStorage.getItem("localStorageData");
-  if (!localData) {
-    return;
-  }
-  try {
-    const {
-      counter = 0,
-      price = 0,
-      quantity = 0,
-      amount = 0,
-    } = JSON.parse(localData);
-    counter.value = counter;
-    price.value = price;
-    quantity.value = quantity;
-    amount.value = amount;
-  } catch (e) {
-    console.log(e);
+  if (localData) {
+    try {
+      const parsed = JSON.parse(localData);
+      counter.value = parsed.counter || 0;
+      price.value = parsed.price || 0;
+      quantity.value = parsed.price || 0;
+      amount.value = parsed.amount || 0;
+    } catch (e) {
+      console.log(e);
+    }
   }
 });
 </script>
@@ -52,7 +127,13 @@ onMounted(() => {
   <div class="container">
     <div class="rows-container">
       <div class="input-row">
-        <input id="price" type="number" placeholder="Цена" v-model="price" />
+        <input
+          id="price"
+          type="number"
+          placeholder="Цена"
+          v-model="price"
+          @input="onChangePrice"
+        />
       </div>
       <div class="input-row">
         <input
@@ -60,13 +141,20 @@ onMounted(() => {
           type="number"
           placeholder="Количество"
           v-model="quantity"
+          @input="onChangeQuantity"
         />
       </div>
       <div class="input-row">
-        <input id="amount" type="number" placeholder="Сумма" v-model="amount" />
+        <input
+          id="amount"
+          type="number"
+          placeholder="Сумма"
+          v-model="amount"
+          @input="onChangeAmount"
+        />
       </div>
       <div class="button">
-        <button>Сохранить</button>
+        <button @click="onSave">Сохранить</button>
       </div>
     </div>
     <div class="labels-container">
@@ -78,9 +166,13 @@ onMounted(() => {
     <div class="actions-container">
       <div class="action">
         <div class="action-label">События</div>
-        <div class="action-header"></div>
-        <div class="action-title"></div>
-        <div class="action-body"></div>
+        <div v-for="event in events" :key="event.id" class="event-item">
+          <div class="action-header">{{ event.time }}</div>
+          <div class="action-title" :class="{ error: success }">
+            {{ event.type }}
+          </div>
+          <div class="action-body">{{ event.body }}</div>
+        </div>
       </div>
     </div>
   </div>
